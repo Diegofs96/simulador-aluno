79991e1af42d298b724722c0c834a1f27cc593e5
Diego Santos <diegoferreira940@gmail.com>
2022-06-21 00:09:27 -0300

    feat: adding producer worker

--- main.go
+++ main.go
@@ -7,19 +7,27 @@ import (
 )
 
 func init() {
-	err :=gotdotenv.Load()
+	err := gotdotenv.Load()
 	if err != nil {
 		log.Fatal("error loading .env file")
 	}
 }
 
 func main() {
-	route := route2.Route{
-		ID:       "1",
-		ClientID: "1",
+	msgchan := make(chan *ckafka.Message)
+	consumer:= kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
 	}
 
-	route.LoadPositions()
-	stringjson, _ := route.ExportJsonPositions()
-	fmt.Println(stringjson[0])
+	// route := route2.Route{
+	// 	ID:       "1",
+	// 	ClientID: "1",
+	// }
+
+	// route.LoadPositions()
+	// stringjson, _ := route.ExportJsonPositions()
+	// fmt.Println(stringjson[0])
 }
